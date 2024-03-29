aefc3eaf63965963ce92157ea4dd1c84714b82c0
Joy Ahmed <dev.joyahmed@gmail.com>
2024-03-30 03:11:46 +0600

    created user schema

--- sweetback/app.js
+++ sweetback/app.js
@@ -1,4 +1,13 @@
 import express from 'express'
 
 const app = express()
-const port = 3000 || proccess.env.PORT
+const PORT = 3000 || proccess.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
