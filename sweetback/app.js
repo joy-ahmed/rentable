56005c97a30281086f5c9f46589debda1341d8fb
Joy Ahmed <dev.joyahmed@gmail.com>
2024-03-30 04:34:21 +0600

    user validation and login

--- sweetback/app.js
+++ sweetback/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import authRoute from './routers/_authRoute.js'
 
 const app = express()
 const PORT = 3000 || proccess.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/auth', authRoute)
--- sweetback/controllers/_authController.js
+++ sweetback/controllers/_authController.js
@@ -1,4 +1,5 @@
 import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -22,20 +23,52 @@ export const register = async (req, res) => {
       },
     });
 
-    if(existingUser) return res.status(400).json({ message: "User already exists" });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
     //hassh password
     const hashedPassword = await bcrypt.hash(password, 10);
     //create user
     const newUser = await prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-        }
-    })
-    console.log(newUser)
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    console.log(newUser);
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) return res.status(401).json({ message: "User does not exist" });
+    //match password
+    const validatedPassword = await bcrypt.compare(password, user.password);
+    if (!validatedPassword)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const expireIn = 1000 * 60 * 60 * 24 * 7;
+    //set jwsonwebtoken inside cookie
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: expireIn }
+    );
+    res
+      .cookie("token", token, { httpOnly: true, maxAge: expireIn })
+      .json({ message: "Login successfully" });
   } catch (error) {
     console.log(error);
   }
